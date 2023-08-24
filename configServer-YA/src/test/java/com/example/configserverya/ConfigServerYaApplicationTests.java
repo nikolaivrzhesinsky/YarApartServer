6a4ccd1562cf4658002491bc6a6d9c9537d8db67
nikolaivrzhesinsky <15nk33l49@gmail.com>
2023-08-24 22:37:07 +0300

    add ConfigServerYaApplication + DiscoveryApplication + GatewayYaApplication

---
+++ configServer-YA/src/test/java/com/example/configserverya/ConfigServerYaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserverya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerYaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gateway-YA/src/main/java/com/example/gateway/GatewayYaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayYaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayYaApplication.class, args);
+    }
+
+}
